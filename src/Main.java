1de8df2e722e792e37019078be7b523d7b376b1e
Michael Kaiser <michael.kaiser@emplabs.ch>
2025-02-20 13:51:56 +0100

    initial push of the repo

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Main class to run everything
+ *
+ * @author Michael Kaiser
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Main run method
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
